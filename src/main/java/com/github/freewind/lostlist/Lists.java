c5df8b49ba51698172ccdc9a3dac20f6238375fb
freewind <nowindlee@gmail.com>
2018-07-26 14:25:28 +0800

    new method: newListFilled

--- src/main/java/com/github/freewind/lostlist/Lists.java
+++ src/main/java/com/github/freewind/lostlist/Lists.java
@@ -3,20 +3,29 @@ package com.github.freewind.lostlist;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 public class Lists {
 
-    public static <T> List<T> emptyList() {
+    public static <T> ArrayList<T> emptyList() {
         return new ArrayList<T>();
     }
 
-    public static <T> List<T> newList(T... items) {
+    public static <T> ArrayList<T> newList(T... items) {
         return new ArrayList<T>(Arrays.asList(items));
     }
 
-    public static <T> List<T> newList(Collection<? extends T> items) {
+    public static <T> ArrayList<T> newList(Collection<? extends T> items) {
         return new ArrayList<T>(items);
+    }
+
+    public static <T> ArrayList<T> newListFilled(int size, T t) {
+        if (size < 0) throw new IllegalArgumentException("size(" + size + ") should >= 0");
+
+        ArrayList<T> list = new ArrayList<T>();
+        for (int i = 0; i < size; i++) {
+            list.add(t);
+        }
+        return list;
     }
 
 }
--- src/test/java/com/github/freewind/lostlist/ListsSpec.java
+++ src/test/java/com/github/freewind/lostlist/ListsSpec.java
@@ -7,49 +7,45 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class ListsSpec {
 
     @Test
     public void test_create_empty_array_list() {
-        List<Object> list = Lists.emptyList();
+        ArrayList<Object> list = Lists.emptyList();
         assertThat(list).isEmpty();
-        assertThat(list).isInstanceOf(ArrayList.class);
     }
 
     @Test
     public void test_create_array_list_with_one_item() {
-        List<String> list = Lists.newList("hello");
+        ArrayList<String> list = Lists.newList("hello");
         assertThat(list).hasSize(1);
         assertThat(list).containsExactly("hello");
-        assertThat(list).isInstanceOf(ArrayList.class);
     }
 
     @Test
     public void test_create_array_list_with_multi_items() {
-        List<String> list = Lists.newList("hello", "world");
+        ArrayList<String> list = Lists.newList("hello", "world");
         assertThat(list).hasSize(2);
         assertThat(list).containsExactly("hello", "world");
-        assertThat(list).isInstanceOf(ArrayList.class);
     }
 
     @Test
     public void test_create_array_list_with_array() {
         String[] items = {"hello", "world"};
-        List<String> list = Lists.newList(items);
+        ArrayList<String> list = Lists.newList(items);
         assertThat(list).hasSize(2);
         assertThat(list).containsExactly("hello", "world");
-        assertThat(list).isInstanceOf(ArrayList.class);
     }
 
     @Test
     public void test_create_array_list_with_another_list() {
         List<String> items = Lists.newList("hello", "world");
-        List<String> list = Lists.newList(items);
+        ArrayList<String> list = Lists.newList(items);
         assertThat(list).hasSize(2);
         assertThat(list).containsExactly("hello", "world");
-        assertThat(list).isInstanceOf(ArrayList.class);
     }
 
     @Test
@@ -58,10 +54,29 @@ public class ListsSpec {
         items.add("hello");
         items.add("world");
 
-        List<String> list = Lists.newList(items);
+        ArrayList<String> list = Lists.newList(items);
         assertThat(list).hasSize(2);
-        assertThat(list).containsExactly("hello", "world");
-        assertThat(list).isInstanceOf(ArrayList.class);
+        assertThat(list).contains("hello", "world");
+    }
+
+    @Test
+    public void test_create_array_list_filled_with_specified_item() {
+        ArrayList<String> list = Lists.newListFilled(3, "a");
+        assertThat(list).hasSize(3);
+        assertThat(list).containsExactly("a", "a", "a");
+    }
+
+    @Test
+    public void test_create_array_list_filled_with_size_0_returns_empty_list() {
+        ArrayList<String> list = Lists.newListFilled(0, "a");
+        assertThat(list).isEmpty();
+    }
+
+    @Test
+    public void test_create_array_list_filled_throws_exception_is_size_is_negative() {
+        assertThatThrownBy(() -> Lists.newListFilled(-1, "a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("size(-1) should >= 0");
     }
 
 }
